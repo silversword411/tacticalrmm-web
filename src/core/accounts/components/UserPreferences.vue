silversword411/tacticalrmm-web

commit a4d7265e373c985de881691c40ebb23677794615

implement new store changes to be more consistent with what pinia was doing and add singleton pattern to prevent circular dependencies. also backport uninstall software UI

src/core/accounts/components/UserPreferences.vue
@@ -207,8 +207,10 @@
 <script setup lang="ts">
 import { ref, reactive, onMounted } from "vue";
 import { openURL, useDialogPluginComponent } from "quasar";
-import { userStore } from "src/stores/api";
-import { useDashboardStore } from "src/stores/dashboard";
+import { useUserStore, useDashboardStore } from "src/stores/api";
+
+const userStore = useUserStore();
+const dashboardStore = useDashboardStore();
 import { useURLActionDropdown } from "src/core/settings/composables";
 
 // type imports
@@ -238,9 +240,6 @@ const loadingBarColors = [
   "grey",
   "blue-grey",
 ];
-
-// setup stores
-const dashboardStore = useDashboardStore();
 
 // dropdowns
 const { webActionOptions } = useURLActionDropdown();

commit 1775709f4ea5834c1303f9bf1726d0483f5e17a4

User Preferences: Fix agent double-click action not saving from bug introduced by a4d7265

src/core/accounts/components/UserPreferences.vue
@@ -27,7 +27,7 @@
                   <div class="col-6">Agent double-click action:</div>
                   <div class="col-2"></div>
                   <q-select
-                    v-model="state.dblclick_action"
+                    v-model="state.agent_dblclick_action"
                     map-options
                     emit-value
                     filled
@@ -38,7 +38,7 @@
                     @update:model-value="state.url_action = null"
                   />
                 </q-card-section>
-                <q-card-section v-if="state.dblclick_action === 'urlaction'" class="row">
+                <q-card-section v-if="state.agent_dblclick_action === 'urlaction'" class="row">
                   <div class="col-6">URL Action:</div>
                   <div class="col-2"></div>
                   <q-select
@@ -251,7 +251,7 @@ const loadingBarColors = [
 const { webActionOptions } = useURLActionDropdown();
 
 const state = reactive<Partial<User>>({
-  dblclick_action: dashboardSettings.agentDblClickAction,
+  agent_dblclick_action: dashboardSettings.agentDblClickAction,
   url_action: dashboardSettings.agentUrlAction,
   default_agent_tbl_tab: dashboardSettings.defaultAgentTblTab,
   client_tree_sort: dashboardSettings.clientTreeSort,

src/core/accounts/types.ts
@@ -16,7 +16,7 @@ export interface User {
   block_dashboard_login?: boolean;
   dark_mode?: boolean;
   show_community_scripts?: boolean;
-  dblclick_action?: AgentDblClickAction;
+  agent_dblclick_action?: AgentDblClickAction;
   url_action?: number | null;
   default_agent_tbl_tab?: AgentTableTab;
   agents_per_page?: number;